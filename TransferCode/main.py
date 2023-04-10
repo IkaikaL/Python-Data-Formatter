826619c6b75f1603f5b0f122752fc8cd378c5b33
Ikaika <leeikaika4@gmail.com>
2023-04-10 12:22:57 -0500

    RandomForestClassifier not finished

--- TransferCode/main.py
+++ TransferCode/main.py
@@ -5,43 +5,68 @@ import pandas as pd
 import sklearn
 import sklearn.ensemble
 
+# Modelling
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
+
+# Tree Visualisation
+from sklearn.tree import export_graphviz
+from IPython.display import Image
+import graphviz
+
 data = pd.read_excel('wslp_f29.xlsx', sheet_name = 0)
 
 
-X = data.loc[: , ['Depth(ft)','qc(psf)','fs(psf)','u2(psf)','log(qt/Pa)','log(Rf)','geology']] # 8 feature columns
-Y = data.loc[: , ['prec']] # Output column
+X = data.loc[: , ['Depth(ft)','qc(psf)','fs(psf)','u2(psf)','log(qt/Pa)','log(Rf)','organic']] # 8 feature columns
+Y = data.loc[: , ['organic']] # Output column
 m = len(Y)
+
 """""
 print(X)
 print(Y)
 print(m)
 """""
+
 PredictorNames = ["depth","qc","fs","u2","log(qt/Pa)","log(Rf)","geology","preconsolidation"]
 
 
 # Randomizing data
 p = .8;
+
 idx = np.random.permutation(m)
 
+print(idx[1:round(p*m)])
+
 xtr = X.loc[idx[1:round(p*m)]]
+
+ytr = Y.loc[idx[1:round(p*m)]]
+
+xte = X.loc[idx[round(p*m)+1:len(idx)-1]]
+
+yte = Y.loc[idx[round(p*m)+1:len(idx)-1]]
+
+"""""
+print(m)
+print(idx)
 print(xtr)
 print(len(xtr))
-ytr = Y.loc[idx[1:round(p*m)]]
 print(ytr)
 print(len(ytr))
-xte = X.loc[idx[round(p*m)+1:len(idx)-1]]
 print(xte)
 print(len(xte))
-yte = Y.loc[idx[round(p*m)+1:len(idx)-1]]
 print(ytr)
 print(len(yte))
-
+"""""
 
 # Create a random forest model
-Mdl = sklearn.ensemble.RandomForestClassifier(100,xtr,ytr,'Method','classification','minleafsize',7,'OOBPred','On','OOBPredictorImportance','on')
+Mdl = sklearn.ensemble.RandomForestClassifier()
+Mdl.fit(xtr, ytr)
 
 hte = Mdl.predict(xte)
-hte = float(hte)
+print(hte)
+#hte = float(hte)
 accuracy_te = statistics.mean(round(hte) == yte)
 confusion_matrix_te = sklearn.metrics.confusion_matrix(yte, round(hte))
 cm_display_te = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_te, display_labels = [False, True])
@@ -63,5 +88,6 @@ confusion_matrix_all = sklearn.metrics.confusion_matrix(ytr, round(htr))
 cm_display_all = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_all, display_labels = [False, True])
 cm_display_all.plot()
 plt.show()
+
 """""
 """""
--- testMatlab.m
+++ testMatlab.m
@@ -11,35 +11,31 @@ m = length(Y);
 
 PredictorNames = ["depth","qc","fs","u2","log(qt/Pa)","log(Rf)","geology","preconsolidation"]';
 
-rng(1) % for reproducibility
+rng(1); % for reproducibility
 
 % Randomizing data
 p = .8;
 idx = randperm(m);
-xtr = X(idx(1:round(p*m)),:)
-length(xtr)
-ytr = Y(idx(1:round(p*m)),:)
-length(ytr)
-xte = X(idx(round(p*m)+1:end),:)
-length(xte)
-yte = Y(idx(round(p*m)+1:end),:)
-length(yte)
+xtr = X(idx(1:round(p*m)),:);
+ytr = Y(idx(1:round(p*m)),:);
+xte = X(idx(round(p*m)+1:end),:);
+yte = Y(idx(round(p*m)+1:end),:);
 
 % Create a random forest model
 Mdl = TreeBagger(100,xtr,ytr,'Method','classification','minleafsize',7,'OOBPred','On',...
     'OOBPredictorImportance','on');
 
-hte = predict(Mdl,xte);
+hte = predict(Mdl,xte)
 hte = str2double(hte);
-accuracy_te = mean(round(hte)==yte); %added ;
-confusionchart(yte,round(hte)); %added ;
+accuracy_te = mean(round(hte)==yte)
+confusionchart(yte,round(hte))
 
 htr = predict(Mdl,xtr);
 htr = str2double(htr);
-accuracy_tr = mean(round(htr)==ytr); %added ;
-confusionchart(ytr,round(htr)); %added ;
+accuracy_tr = mean(round(htr)==ytr)
+confusionchart(ytr,round(htr))
 
 htall = predict(Mdl,X);
 htall = str2double(htall);
-accuracy_all = mean(round(htall)==Y); %added ;
-confusionchart(Y,round(htall)); %added ;
+accuracy_all = mean(round(htall)==Y)
+confusionchart(Y,round(htall))
