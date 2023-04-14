282b05877dcca4bebccf930dc206ba445f0350f0
Ikaika <leeikaika4@gmail.com>
2023-04-14 09:42:19 -0500

    finished pending omar approval

--- TransferCode/main.py
+++ TransferCode/main.py
@@ -23,12 +23,6 @@ X = data.loc[: , ['Depth(ft)','qc(psf)','fs(psf)','u2(psf)','log(qt/Pa)','log(Rf
 Y = data.loc[: , ['organic']] # Output column
 m = len(Y)
 
-"""""
-print(X)
-print(Y)
-print(m)
-"""""
-
 PredictorNames = ["depth","qc","fs","u2","log(qt/Pa)","log(Rf)","geology","preconsolidation"]
 
 
@@ -45,57 +39,29 @@ xte = X.loc[idx[round(p*m)+1:len(idx)-1]]
 
 yte = Y.loc[idx[round(p*m)+1:len(idx)-1]]
 
-"""""
-print(m)
-print(idx)
-print(xtr)
-print(len(xtr))
-print(ytr)
-print(len(ytr))
-print(xte)
-print(len(xte))
-print(ytr)
-print(len(yte))
-"""""
-
 # Create a random forest model
 Mdl = sklearn.ensemble.RandomForestClassifier()
-
 Mdl.fit(xtr, np.ravel(ytr))
 
 hte = Mdl.predict(xte)
-print(hte)
 
-yPredict = Mdl.predict(xte)
-print(yPredict)
-
-accuracy = accuracy_score(yte, yPredict)
+accuracy = accuracy_score(yte, hte)
 print(accuracy)
 
-confusionMatrix = confusion_matrix(yte, yPredict)
-ConfusionMatrixDisplay(confusion_matrix = confusionMatrix).plot()
-
-"""""
-#hte = float(hte)
-accuracy_te = statistics.mean(round(hte) == yte)
-confusion_matrix_te = sklearn.metrics.confusion_matrix(yte, round(hte))
-cm_display_te = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_te, display_labels = [False, True])
-cm_display_te.plot()
+confusionMatrix = confusion_matrix(yte, hte)
+ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 plt.show()
 
 htr = Mdl.predict(xtr)
-htr = float(htr)
-accuracy_tr = statistics.mean(round(htr) == ytr)
-confusion_matrix_tr = sklearn.metrics.confusion_matrix(ytr, round(htr))
-cm_display_tr = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_tr, display_labels = [False, True])
-cm_display_tr.plot()
+accuracy = accuracy_score(ytr, htr)
+
+confusionMatrix = confusion_matrix(ytr, htr)
+ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 plt.show()
 
-htall = Mdl.predict(X)
-htall = float(htall)
-accuracy_all = statistics.mean(round(htall) == Y)
-confusion_matrix_all = sklearn.metrics.confusion_matrix(ytr, round(htr))
-cm_display_all = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_all, display_labels = [False, True])
-cm_display_all.plot()
+htAll = Mdl.predict(X)
+accuracy = accuracy_score(Y, htAll)
+
+confusionMatrix = confusion_matrix(Y, htAll)
+ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 plt.show()
-"""""
