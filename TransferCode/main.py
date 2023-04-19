1d8df3b084273322e45f417acfce37de6736f8c3
Ikaika Lee <willtolivehaver@gmail.com>
2023-04-19 17:57:33 -0500

    data uploaded and formatted correctly ready to run through model

--- TransferCode/main.py
+++ TransferCode/main.py
@@ -18,67 +18,65 @@ import graphviz
 
 data = pd.read_excel('wslp_f29.xlsx', sheet_name=0)
 
-newData = pd.read_excel('WSLP-101.xlsx', header=8, sheet_name=1, skiprows=[9])
-#index_col= [9, 10, 13, 15, 20, 43, 44],
+newData = pd.read_excel('WSLP-101.xlsx', header=8, sheet_name=1, skiprows=[9, 10, 11])
 
+
+goodData = newData.loc[:, ["Depth ", "qc", 'fs', 'u2', 'qt/pa', 'Rf', "s 'p"]]
+goodData = goodData.dropna()
 
 X = data.loc[:, ['Depth(ft)', 'qc(psf)', 'fs(psf)', 'u2(psf)', 'log(qt/Pa)', 'log(Rf)', 'geology', 'prec']]  # 8 feature columns
 Y = data.loc[:, ['organic']]  # Output column
 m = len(Y)
 
-print(newData)
+goodData.iloc[:, [1]] *= 2000
 
-depthValues = newData.iloc[:, [9]]
-print(depthValues)
+goodData.iloc[:, [2]] *= 2000
 
-qcValues = newData.iloc[:, [10]]
-qcValues = qcValues * 2000
-print(qcValues)
+goodData.iloc[:, [3]] *= 2000
 
-fsValues = newData.iloc[:, [13]]
-print(fsValues)
+goodData['qt/pa'] = np.log10(goodData['qt/pa'])
 
-u2Values = newData.iloc[:, [15]]
-print(u2Values)
+goodData['Rf'] = np.log10(goodData['Rf'])
 
-preConsolidationValues = newData.iloc[:, [20]]
-preConsolidationValues = preConsolidationValues * 2000
-print(preConsolidationValues)
+geoValues = np.zeros((len(goodData), 1))
+goodData['geology'] = geoValues
 
-qtPaValues = newData.iloc[:, [43]]
-#qtPaValues = np.log10(qtPaValues)
-print(qtPaValues)
-
-rfValues = newData.iloc[:, [44]]
-print(rfValues)
-
-newDataFinished = depthValues
-newDataFinished = np.append(qcValues, axis=1)
-print(newDataFinished)
-
-
-'''''
-PredictorNames = ["depth", "qc", "fs", "u2", "log(qt/Pa)", "log(Rf)", "geology", "preconsolidation"]
-
+print(goodData)
 
 # Randomizing data
 p = .8
 
 idx = np.random.permutation(m)
+idxTest = np.random.permutation(len(goodData))
+
 
 xtr = X.loc[idx[1:round(p*m)]]
+xtrTest = goodData.loc[idxTest[1:round(p*m)]]
 
 ytr = Y.loc[idx[1:round(p*m)]]
+#ytrTest =
 
 xte = X.loc[idx[round(p*m)+1:len(idx)-1]]
+xteTest = goodData.loc[idxTest[round(p*m)+1:len(idxTest)-1]]
 
 yte = Y.loc[idx[round(p*m)+1:len(idx)-1]]
+#yteTest =
 
+
+print(idx)
+print(xtr)
+print(ytr)
+print(xte)
+print(yte)
+'''''
+'''''
 # Create a random forest model
 Mdl = sklearn.ensemble.RandomForestClassifier()
 Mdl.fit(xtr, np.ravel(ytr))
+Mdl.fit(xtrTest)
 
 hte = Mdl.predict(xte)
+hteTest = Mdl.predict(xteTest)
 
 accuracy = accuracy_score(yte, hte)
 
@@ -86,6 +84,7 @@ confusionMatrix = confusion_matrix(yte, hte)
 ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 
 htr = Mdl.predict(xtr)
+htrTest = Mdl.predict(xtrTest)
 accuracy = accuracy_score(ytr, htr)
 
 confusionMatrix = confusion_matrix(ytr, htr)
@@ -97,5 +96,6 @@ accuracy = accuracy_score(Y, htAll)
 confusionMatrix = confusion_matrix(Y, htAll)
 ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 
-plt.show()
+#plt.show()
+'''''
 '''''
