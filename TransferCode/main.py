ab47e354f71a87268d9ae6d7cfabe7069646e87e
Ikaika Lee <willtolivehaver@gmail.com>
2023-04-27 13:21:18 -0500

    outputting elevation and predictions to excel file

--- TransferCode/main.py
+++ TransferCode/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import sklearn
 import sklearn.ensemble
+import openpyxl
 
 # Modelling
 from sklearn.ensemble import RandomForestClassifier
@@ -28,13 +29,23 @@ goodDataWslp101 = wslp101.loc[:, ["Depth ", "qc", 'fs', 'u2', 'qt/pa', 'Rf', "s 
 goodDataWslp101 = goodDataWslp101.rename(columns={"s 'p": "prec"})
 goodDataWslp101 = goodDataWslp101.dropna()
 
+elevationWslp101 = wslp101.loc[:, ["Elevation"]]
+elevationWslp101 = elevationWslp101.dropna()
+
 goodDataWslp108 = wslp108.loc[:, ["Depth", "q_c", "fs", "Pw", "q_t", "Rf", "Total Stress"]]
-goodDataWslp108 = goodDataWslp108.rename(columns={"q_c": "qc","Pw": "u2", "q_t": "qt", "Total Stress": "prec"})
+goodDataWslp108 = goodDataWslp108.rename(columns={"Depth": "Depth ","q_c": "qc","Pw": "u2", "q_t": "qt/pa", "Total Stress": "prec"})
 goodDataWslp108 = goodDataWslp108.dropna()
+
+elevationWslp108 = wslp108.loc[:, ["Elevation"]]
+elevationWslp108 = elevationWslp108.dropna()
 
 goodDataWslp109 = wslp109.loc[:, ["Depth ", "qc", "fs", "u2", "qt/pa", "Rf", "sp"]]
 goodDataWslp109 = goodDataWslp109.rename(columns={"sp": "prec"})
 goodDataWslp109 = goodDataWslp109.dropna()
+
+elevationWslp109 = wslp109.loc[:, ["Elevation"]]
+elevationWslp109 = elevationWslp109.dropna()
+
 
 # formatting numbers
 goodDataWslp101.iloc[:, [1]] *= 2000
@@ -53,9 +64,9 @@ goodDataWslp108.iloc[:, [1]] *= 2000
 goodDataWslp108.iloc[:, [2]] *= 2000
 goodDataWslp108.iloc[:, [3]] *= 144
 goodDataWslp108.iloc[:, [4]] /= 1.0581
-goodDataWslp108['prec'] = .33 * (5 - goodDataWslp108['qt']) * 2000 - goodDataWslp108['prec']
+goodDataWslp108['prec'] = .33 * (5 - goodDataWslp108['qt/pa']) * 2000 - goodDataWslp108['prec']
 
-goodDataWslp108['qt'] = np.log10(goodDataWslp108['qt'])
+goodDataWslp108['qt/pa'] = np.log10(goodDataWslp108['qt/pa'])
 goodDataWslp108['Rf'] = np.log10(goodDataWslp108['Rf'])
 
 # creating and inserting geology column
@@ -109,21 +120,26 @@ np.random.seed(0)
 p = .8
 
 idx = np.random.permutation(m)
-idxTest = np.random.permutation(len(goodDataWslp101))
-
+idxWslp101 = np.random.permutation(len(goodDataWslp101))
+idxWslp108 = np.random.permutation(len(goodDataWslp108))
+idxWslp109 = np.random.permutation(len(goodDataWslp109))
 
 xtr = X.loc[idx[1:round(p*m)]]
 
 ytr = Y.loc[idx[1:round(p*m)]]
 
 xte = X.loc[idx[round(p*m)+1:len(idx)-1]]
-newDataTest = goodDataWslp101.loc[idxTest[round(p*dataLengthWslp101)+1:len(idxTest)-1]]
+newDataTestWslp101 = goodDataWslp101.loc[idxWslp101[round(p*dataLengthWslp101)+1:len(idxWslp101)-1]]
+newDataTestWslp108 = goodDataWslp108.loc[idxWslp108[round(p*dataLengthWslp108)+1:len(idxWslp108)-1]]
+newDataTestWslp109 = goodDataWslp109.loc[idxWslp109[round(p*dataLengthWslp109)+1:len(idxWslp109)-1]]
+
+trimmedElevationWslp101 = elevationWslp101.loc[idxWslp101[round(p*dataLengthWslp101)+1:len(idxWslp101)-1]]
+trimmedElevationWslp108 = elevationWslp108.loc[idxWslp108[round(p*dataLengthWslp108)+1:len(idxWslp108)-1]]
+trimmedElevationWslp109 = elevationWslp109.loc[idxWslp109[round(p*dataLengthWslp109)+1:len(idxWslp109)-1]]
+
 
 yte = Y.loc[idx[round(p*m)+1:len(idx)-1]]
 #newDataTestSampleAnswers = testAnswers.loc[idxTest[round(p*dataLengthWslp101)+1:len(idxTest)-1]]
-
-print(newDataTest)
-#print(newDataTestSampleAnswers)
 
 
 # Create a random forest model
@@ -135,28 +151,50 @@ hte = Mdl.predict(xte)
 accuracy = accuracy_score(yte, hte)
 
 confusionMatrix = confusion_matrix(yte, hte)
-ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
+#ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 
 htr = Mdl.predict(xtr)
 accuracy = accuracy_score(ytr, htr)
 
 confusionMatrix = confusion_matrix(ytr, htr)
-ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
+#ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 
 htAll = Mdl.predict(X)
 accuracy = accuracy_score(Y, htAll)
 
 confusionMatrix = confusion_matrix(Y, htAll)
-ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
+#ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
 
 
-newDataPrediction = Mdl.predict(newDataTest)
+newDataPredictionWslp101 = Mdl.predict(newDataTestWslp101)
 #accuracy = accuracy_score(newDataPrediction, newDataTestSampleAnswers)
-print(newDataPrediction)
+print(newDataPredictionWslp101)
 
-#confusionMatrix = confusion_matrix(newDataPrediction, newDataTestSampleAnswers)
-ConfusionMatrixDisplay(confusion_matrix=confusionMatrix).plot()
+newDataPredictionWslp108 = Mdl.predict(newDataTestWslp108)
+#accuracy = accuracy_score(newDataPrediction, newDataTestSampleAnswers)
+print(newDataPredictionWslp108)
 
-#plt.show()
+newDataPredictionWslp109 = Mdl.predict(newDataTestWslp109)
+#accuracy = accuracy_score(newDataPrediction, newDataTestSampleAnswers)
+print(newDataPredictionWslp109)
+
+predictionDataFrameWslp101 = pd.DataFrame(newDataPredictionWslp101, index=trimmedElevationWslp101.index)
+predictionDataFrameWslp101 = predictionDataFrameWslp101.rename(columns={0: 'pred'})
+outputDataWslp101 = pd.concat([trimmedElevationWslp101, predictionDataFrameWslp101], axis=1)
+
+predictionDataFrameWslp108 = pd.DataFrame(newDataPredictionWslp108, index=trimmedElevationWslp108.index)
+predictionDataFrameWslp108 = predictionDataFrameWslp108.rename(columns={0: 'pred'})
+outputDataWslp108 = pd.concat([trimmedElevationWslp108, predictionDataFrameWslp108], axis=1)
+
+predictionDataFrameWslp109 = pd.DataFrame(newDataPredictionWslp109, index=trimmedElevationWslp109.index)
+predictionDataFrameWslp109 = predictionDataFrameWslp109.rename(columns={0: 'pred'})
+outputDataWslp109 = pd.concat([trimmedElevationWslp109, predictionDataFrameWslp109], axis=1)
+
+
+with pd.ExcelWriter('outputData.xlsx') as writer:
+    outputDataWslp101.to_excel(writer, sheet_name='WSLP-101')
+    outputDataWslp108.to_excel(writer, sheet_name='WSLP-108')
+    outputDataWslp109.to_excel(writer, sheet_name='WSLP-109')
+
 '''''
 '''''
